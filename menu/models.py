da71bebb2c9e1db8976230dc0bf38496369c77c3
Tes3103 <>
2023-11-01 17:12:57 +0000

    Add menu and favourite class

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.conf import settings
 
-# Create your models here.
+
+class Meal(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    cover = CloudinaryField('image')
+    price = models.IntegerField()
+
+    def _str_(self):
+        return self.name
+
+
+class Favourite(models.Model):
+    user = models.ForeignKey(Meal, on_delete=models.CASCADE,
+                             related_name='choosen_food', blank=True)
+    meal = models.ForeignKey(
+        Meal, on_delete=models.CASCADE, related_name='favourite', blank=True)
+
+    class Meta:
+        ordering = ['meal']
+
+
+    def _str_(self):
+        return self.user
